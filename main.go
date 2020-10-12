8e7dded860b4dadb36d59b2ca50ebd8a36a55ec8
Isaac Ramirez <isaac.ramirez.herrera@gmail.com>
2020-10-11 19:18:47 -0600

    First release

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "First working API in Go!")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+
+	log.Println("Server started")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
